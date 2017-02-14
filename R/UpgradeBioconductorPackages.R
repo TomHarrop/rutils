89806cd480812c8f8e86682182a38c8754e77ecb
Tom Harrop <twharrop@gmail.com>
2017-02-15 11:41:22 +1300

    Choose a closer mirror for bioc

--- R/UpgradeBioconductorPackages.R
+++ R/UpgradeBioconductorPackages.R
@@ -6,6 +6,8 @@
 
 UpgradeBioconductorPackages <- function(){
   if(!exists("biocLite")){
+    # use an AU HTTP mirror (may download quicker)
+    chooseBioCmirror(ind = 13)
     message("Sourcing biocLite()")
     source("https://bioconductor.org/biocLite.R")
   }
